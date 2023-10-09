ad818060c7ef21343248b243c0a12724f14ce223
janvi kachchhi007 <janukachchhi2@gmail.com>
2023-10-09 15:49:16 +0530

    new

--- lib/screens/quotes_edit_screen.dart
+++ lib/screens/quotes_edit_screen.dart
@@ -4,6 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:like_button/like_button.dart';
 import 'package:quotes_app/utils/colors.dart';
 import 'package:quotes_app/utils/global.dart';
+import 'package:quotes_app/utils/img_list.dart';
 
 import '../model/quotes_model.dart';
 
@@ -19,10 +20,9 @@ class _QuotesEditScreenState extends State<QuotesEditScreen> {
   bool bold = true;
   bool italic = true;
   bool text = true;
-  int i = 0;
-  int j = 14;
   TextAlign txtAlign = TextAlign.center;
   int colorIndex = 0, fontColor = 0;
+  int image = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -62,167 +62,204 @@ class _QuotesEditScreenState extends State<QuotesEditScreen> {
                     },
                   );
                 },
-                child: Container(
-                  height: MediaQuery.of(context).size.height * 0.75,
-                  width: MediaQuery.of(context).size.width * 0.95,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    color: colors[colorIndex],
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "${model.quotes}",
-                        textAlign: txtAlign,
-                        style: TextStyle(
-                            fontWeight:
-                                bold ? FontWeight.bold : FontWeight.normal,
-                            fontStyle:
-                                italic ? FontStyle.italic : FontStyle.normal,
-                            fontSize: 19,
-                            color: colors[fontColor],
-                            fontFamily:
-                                text ? 'satisfy' : 'playfairdisplay400'),
-                      ),
-                      SizedBox(height: 50),
-                      Text(
-                        "${model.author}",
-                        textAlign: txtAlign,
-                        style: GoogleFonts.playfairDisplay(
-                          fontWeight: FontWeight.bold,
-                          fontStyle: FontStyle.italic,
-                          color: colors[colorIndex],
+                child: Stack(children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.55,
+                    width: MediaQuery.of(context).size.width * 0.95,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: colors[colorIndex],
+                      image:DecorationImage(image:AssetImage("assets/images/bg/${imgList[image]}"),fit: BoxFit.cover)
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "${model.quotes}",
+                          textAlign: txtAlign,
+                          style: TextStyle(
+                              fontWeight:
+                                  bold ? FontWeight.bold : FontWeight.normal,
+                              fontStyle:
+                                  italic ? FontStyle.italic : FontStyle.normal,
+                              fontSize: 19,
+                              color: colors[fontColor],
+                              fontFamily:
+                                  text ? 'satisfy' : 'playfairdisplay400'),
                         ),
-                      )
-                    ],
+                        SizedBox(height: 50),
+                        Text(
+                          "${model.author}",
+                          textAlign: txtAlign,
+                          style: GoogleFonts.playfairDisplay(
+                            fontWeight: FontWeight.bold,
+                            fontStyle: FontStyle.italic,
+                            color: colors[fontColor],
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                ),
+                ]),
               ),
               Spacer(),
-              Column(
+              Stack(
                 children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.10,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.white),
-                    child: SingleChildScrollView(
-                      scrollDirection: Axis.horizontal,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.download_outlined,
-                              color: Colors.black,
+                  Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.30,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white),
+                        child: Column(
+                          children: [
+                            SingleChildScrollView(scrollDirection: Axis.horizontal,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.download_outlined,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        Clipboard.setData(ClipboardData(
+                                            text: "${model.quotes}"));
+                                      });
+                                    },
+                                    icon: Icon(
+                                      Icons.copy,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        if (fontColor < colors.length - 1) {
+                                          fontColor++;
+                                        } else {
+                                          fontColor = 0;
+                                        }
+                                      });
+                                    },
+                                    icon: Icon(
+                                      Icons.color_lens_outlined,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        bold = !bold;
+                                      });
+                                    },
+                                    icon: Icon(
+                                      Icons.format_bold,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        italic = !italic;
+                                      });
+                                    },
+                                    icon: Icon(
+                                      Icons.format_italic,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        text = !text;
+                                      });
+                                    },
+                                    icon: Icon(
+                                      Icons.text_format,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.share,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        txtAlign = TextAlign.left;
+                                      });
+                                    },
+                                    icon: Icon(
+                                      Icons.format_align_left_outlined,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        txtAlign = TextAlign.center;
+                                      });
+                                    },
+                                    icon: Icon(
+                                      Icons.format_align_center_outlined,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        txtAlign = TextAlign.right;
+                                      });
+                                    },
+                                    icon: Icon(
+                                      Icons.format_align_right_outlined,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              setState(() {
-                                Clipboard.setData(
-                                    ClipboardData(text: "${model.quotes}"));
-                              });
-                            },
-                            icon: Icon(
-                              Icons.copy,
-                              color: Colors.black,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              setState(() {
-                                if (fontColor < colors.length - 1) {
-                                  fontColor++;
-                                } else {
-                                  fontColor = 0;
-                                }
-                              });
-                            },
-                            icon: Icon(
-                              Icons.color_lens_outlined,
-                              color: Colors.black,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              setState(() {
-                                bold = !bold;
-                              });
-                            },
-                            icon: Icon(
-                              Icons.format_bold,
-                              color: Colors.black,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              setState(() {
-                                italic = !italic;
-                              });
-                            },
-                            icon: Icon(
-                              Icons.format_italic,
-                              color: Colors.black,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              setState(() {
-                                text = !text;
-                              });
-                            },
-                            icon: Icon(
-                              Icons.text_format,
-                              color: Colors.black,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.share,
-                              color: Colors.black,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              setState(() {
-                                txtAlign = TextAlign.left;
-                              });
-                            },
-                            icon: Icon(
-                              Icons.format_align_left_outlined,
-                              color: Colors.black,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              setState(() {
-                                txtAlign = TextAlign.center;
-                              });
-                            },
-                            icon: Icon(
-                              Icons.format_align_center_outlined,
-                              color: Colors.black,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              setState(() {
-                                txtAlign = TextAlign.right;
-                              });
-                            },
-                            icon: Icon(
-                              Icons.format_align_right_outlined,
-                              color: Colors.black,
-                            ),
-                          ),
-                        ],
+                            SizedBox(
+                              height: 100,
+                              child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: imgList.length,
+                                itemExtent: 100,
+                                itemBuilder: (BuildContext context, int index) {
+                                  return Container(
+                                      margin: EdgeInsets.all(5),
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.2,
+                                      width: MediaQuery.of(context).size.width *
+                                          0.2,
+                                      child: InkWell(
+                                          onTap: () {
+                                            setState(() {
+                                              image = index;
+                                            });
+                                          },
+                                          child: Image.asset(
+                                            "assets/images/bg/${imgList[index]}",
+                                            fit: BoxFit.cover,
+                                          )));
+                                },
+                              ),
+                            )
+                          ],
+                        ),
                       ),
-                    ),
+                    ],
                   ),
                 ],
               ),
---
+++ lib/utils/img_list.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+List imgList=[
+"imag1.jpg",
+"imag2.jpg",
+"imag3.jpg",
+"imag4.jpg",
+"imag5.jpg",
+"imag6.jpg",
+"imag7.jpg",
+"imag8.jpg",
+];
